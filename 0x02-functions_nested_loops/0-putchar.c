c5b248e5b76af58ba7b867c2781ce6e5b5782f73
root <root@DESKTOP-OPGM2GL>
2023-06-20 16:19:12 -0700

    Writing C functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,22 @@
-betty-style: no input files
+#include "main.h" 
+
+ /*
+ * File: 0-putchar.c 
+ * main-print putchar followed by a new line
+ * 
+ *Return :always 0
+ */
+  int main(void) 
+ {
+      _putchar('_' );
+      _putchar ('p' );
+      _putchar ('u' );
+      _putchar ('t' );
+      _putchar ('c' );
+      _putchar ('h' );
+      _putchar ('a' );
+      _putchar ('r' );
+      _putchar ('\n');
+      
+       return (0) ;
+} 
