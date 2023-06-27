fc926e5ad191c9166577ac8f03a3e90ccd710af3
root <root@DESKTOP-OPGM2GL>
2023-06-26 17:38:11 -0700

    Functions prototype

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/*
+* author: @deveopsgenuis 
+* file: main.h
+* all the necessary function prototypes
+*/
+
+void reset_to_98(int *n);
+
+#endif /*MAIN_H*/
